4d3b26de04812a0550c38ac222194fa7f3bb29bd
Daria Romanova <d.e.romanova@mail.ru>
2022-02-16 13:46:05 +0300

    Добавлен чат-бот

--- Documents/5.React_projects/myapp/src/App.js
+++ Documents/5.React_projects/myapp/src/App.js
@@ -1,16 +1,64 @@
 import './App.css';
-import Message from './Message';
+import React, { useEffect, useState } from 'react';
+import { AUTHORS, BOTANSWERS } from './constants/common';
+
+
 
 function App(props) {
-  const msg = "Это сообщение передается в дочерний элемент. Ему заданы инлайн-стили"
+  const [messageList, setList] = useState([]);
+  const [value, setValue] = useState('');
+   
+  const handleValue = (event) => {
+    setValue(event.target.value);
+  }
+
+  const updateList = () => {
+    if (value !== '') {
+      setList([...messageList, {
+        text: value,
+        author: AUTHORS.me
+      }]);
+    }
+    setValue ('')
+  }
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      updateList();
+    }
+  }
+
+  useEffect(()=>{
+    let timer
+    let botanswer = BOTANSWERS[Math.floor(Math.random() * BOTANSWERS.length)]
+    if (messageList.length>0 && messageList[messageList.length-1].author === AUTHORS.me){
+      timer = setInterval(() => {
+        setList([...messageList, {
+          text: botanswer,
+          author: AUTHORS.bot
+        }]);
+      }, 1500)
+    }
+    return () => {clearInterval(timer);
+      }
+  }, [messageList])
+  
   return (
     <div className="App">
       <header className="App-header">
-        My First React App
+        Chat Bot
       </header>
         <h3 className="App-greating">Hello, {props.name}!</h3>
-        <Message message = {msg} />
-      
+        <ul className='message_list'>
+          {messageList.map((item) => (
+            <li className='message_box'>
+              <div className='message_text'>{item.text}</div>
+              <div className='message_author'>{item.author}</div>
+            </li>
+          ))}
+        </ul>
+            <input className='message_field' type="text" name="author" value={value} onChange={handleValue} onKeyDown={handleKeyDown}/>
+            <button className='message_button' type="button" onClick={updateList}>Send</button>
     </div>
   );
  }
--- Documents/5.React_projects/myapp/src/index.js
+++ Documents/5.React_projects/myapp/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const myName = 'Daria';
+const myName = 'User';
 
 ReactDOM.render(
  <React.StrictMode>
